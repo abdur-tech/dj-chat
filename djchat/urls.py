2f58d6035f50425d10088a1825e1e725444beea0
abdur-tech <1622abdurrahmaan@gmail.com>
2024-07-10 22:40:20 +0530

    latest

--- djchat/urls.py
+++ djchat/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('core.urls')),
@@ -8,3 +9,5 @@ urlpatterns = [
     path('', include('account.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
